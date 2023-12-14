3915a4c782f71602f1be090139964db0c9376cf0
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-12-14 17:51:42 +0000

    add placeholder elements for folder select tests

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Sidebar, VariantForm } from './components'
 
 function App() {
   const [selectedVariants, setSelectedVariants] = useState([])
 
+    async function handleFolder() {
+      const filePath = await window.electronAPI.openFolder()
+      console.log(filePath)
+    }
+
   return (
     <>
       <h1>ElDewrito Voting JSON Builder</h1>
+      <button id='button' onClick={handleFolder}>open folder</button>
       <Sidebar />
       <div className="container">
         {selectedVariants.map((i) => {
