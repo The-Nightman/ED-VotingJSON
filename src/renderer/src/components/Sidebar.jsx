87c60fcb51f62fccf41b6b24eaa9dc79de2be61c
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-12-14 01:01:35 +0000

    add placeholder arrays and maps for sidebar items

--- src/renderer/src/components/Sidebar.jsx
+++ src/renderer/src/components/Sidebar.jsx
@@ -1,16 +1,37 @@
 import { IoIosArrowBack } from 'react-icons/io'
 
 export function Sidebar() {
+  const variants = []
+  const maps = []
   return (
     <section aria-label="maps and variants sidebar" className="sidebar">
-        <IoIosArrowBack className="sidebarArrow"/>
-      <div>
+      <IoIosArrowBack className="sidebarArrow" />
+      <div className="sidebarVariants">
         <h2>VARIANTS</h2>
-        <ul></ul>
+        <ul>
+          {variants.map((i, index) => {
+            return (
+              <li>
+                <label>
+                  <input type="checkbox" name={index} id={index} />
+                  {i}
+                </label>
+              </li>
+            )
+          })}
+        </ul>
       </div>
-      <div>
+      <div className="sidebarMaps">
         <h2>MAPS</h2>
-        <ul></ul>
+        <ul>
+          {maps.map((i) => {
+            return (
+              <li>
+                <p>{i}</p>
+              </li>
+            )
+          })}
+        </ul>
       </div>
     </section>
   )
