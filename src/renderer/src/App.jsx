ba57e8e358aefa2c8b8b30ae0c438331cedad832
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-12-14 01:05:17 +0000

    add components to app render and gui layout

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,7 +1,19 @@
+import { useState } from 'react'
+import { Sidebar, VariantForm } from './components'
+
 function App() {
+  const [selectedVariants, setSelectedVariants] = useState([])
+
   return (
-  <>
-  </>  
+    <>
+      <h1>ElDewrito Voting JSON Builder</h1>
+      <Sidebar />
+      <div className="container">
+        {selectedVariants.map((i) => {
+          return <VariantForm />
+        })}
+      </div>
+    </>
   )
 }
 
