dc23abd640f26ea3fb7019e496dea14a62480cd3
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-12-15 17:26:25 +0000

    convert sidebar with framer motion into animated accessible sidebar

--- src/renderer/src/components/Sidebar.jsx
+++ src/renderer/src/components/Sidebar.jsx
@@ -1,7 +1,22 @@
+import { useState } from 'react'
 import { IoIosArrowBack } from 'react-icons/io'
+import { AnimatePresence, motion } from 'framer-motion'
 
 export function Sidebar({ data, jsonData, setJsonData }) {
   const { maps, variants } = data
+
+  const [menuState, setMenuState] = useState(false)
+
+  const animVariants = {
+    menu: {
+      closed: { x: '16rem' },
+      open: { x: '0rem' }
+    },
+    button: {
+      closed: { x: '0rem', rotate: 0 },
+      open: { x: '-16rem', rotate: 180 }
+    }
+  }
 
   const handleSelectVariants = (e) => {
     const { value } = e.target
@@ -24,40 +39,67 @@ export function Sidebar({ data, jsonData, setJsonData }) {
   }
 
   return (
-    <section aria-label="maps and variants sidebar" className="sidebar">
-      <IoIosArrowBack className="sidebarArrow" />
-      <div className="sidebarVariants">
-        <h2>GAMETYPES</h2>
-        <ul>
-          {variants.map((i) => {
-            return (
-              <li>
-                <label>
-                  <input
-                    type="checkbox"
-                    name={`checkbox_${i}`}
-                    value={i}
-                    onChange={handleSelectVariants}
-                  />
-                  {i}
-                </label>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
-      <div className="sidebarMaps">
-        <h2>MAPS</h2>
-        <ul>
-          {maps.map((i) => {
-            return (
-              <li>
-                <p>{i}</p>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
-    </section>
+    <>
+      <motion.div
+        role="button"
+        tabIndex={0}
+        className="sidebarArrowCont"
+        animate={menuState ? 'open' : 'closed'}
+        variants={animVariants.button}
+        transition={{ ease: 'linear', duration: 0.15 }}
+        onClick={() => setMenuState(!menuState)}
+        onKeyDown={() => setMenuState(!menuState)}
+      >
+        <IoIosArrowBack className="sidebarArrow" />
+      </motion.div>
+      <AnimatePresence>
+        {menuState && (
+          <motion.section
+            aria-label="maps and variants sidebar"
+            className="sidebar"
+            initial="closed"
+            animate="open"
+            exit="closed"
+            variants={animVariants.menu}
+            transition={{ ease: 'linear', duration: 0.15 }}
+          >
+            <div className="sidebarContainer">
+              <div className="sidebarVariants" role="menubar">
+                <h2>GAMETYPES</h2>
+                <ul>
+                  {variants.map((i) => {
+                    return (
+                      <li>
+                        <label>
+                          <input
+                            type="checkbox"
+                            name={`checkbox_${i}`}
+                            value={i}
+                            onChange={handleSelectVariants}
+                          />
+                          {i}
+                        </label>
+                      </li>
+                    )
+                  })}
+                </ul>
+              </div>
+              <div className="sidebarMaps">
+                <h2>MAPS</h2>
+                <ul>
+                  {maps.map((i) => {
+                    return (
+                      <li>
+                        <p>{i}</p>
+                      </li>
+                    )
+                  })}
+                </ul>
+              </div>
+            </div>
+          </motion.section>
+        )}
+      </AnimatePresence>
+    </>
   )
 }
