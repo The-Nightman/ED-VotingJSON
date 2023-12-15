93f2d11e7a927cd778a47a9d31ba017dcde08579
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-12-15 00:26:28 +0000

    add appropriate state datapoints to sidebar elements and state data handling function for selected variants via checkbox

--- src/renderer/src/components/Sidebar.jsx
+++ src/renderer/src/components/Sidebar.jsx
@@ -1,19 +1,44 @@
 import { IoIosArrowBack } from 'react-icons/io'
 
-export function Sidebar() {
-  const variants = []
-  const maps = []
+export function Sidebar({ data, jsonData, setJsonData }) {
+  const { maps, variants } = data
+
+  const handleSelectVariants = (e) => {
+    const { value } = e.target
+    const index = jsonData.Types.findIndex((i) => i.typeName === value)
+    if (index > -1) {
+      setJsonData({ ...jsonData, Types: jsonData.Types.filter((i) => i.typeName !== value) })
+    } else {
+      const typeObj = {
+        displayName: value,
+        typeName: value,
+        commands: [
+          'Server.SprintEnabled 0',
+          'Server.UnlimitedSprint 0',
+          'Server.AssassinationEnabled 0'
+        ],
+        SpecificMaps: []
+      }
+      setJsonData({ ...jsonData, Types: [...jsonData.Types, typeObj] })
+    }
+  }
+
   return (
     <section aria-label="maps and variants sidebar" className="sidebar">
       <IoIosArrowBack className="sidebarArrow" />
       <div className="sidebarVariants">
-        <h2>VARIANTS</h2>
+        <h2>GAMETYPES</h2>
         <ul>
-          {variants.map((i, index) => {
+          {variants.map((i) => {
             return (
               <li>
                 <label>
-                  <input type="checkbox" name={index} id={index} />
+                  <input
+                    type="checkbox"
+                    name={`checkbox_${i}`}
+                    value={i}
+                    onChange={handleSelectVariants}
+                  />
                   {i}
                 </label>
               </li>
